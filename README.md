bad5f9bb5b0cb965d3caf7117700ef359355d42e
Martins Vanags <martins.vanags>
2023-06-10 21:56:02 +0300

    Updated readme and sql file

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 Created using php 8.2.5
 
+Database schema created using mysql 5.7.33
+
+Requirements:
+
+* php >8.0
+* mysql 5.7.X
+
 Installation:
+
 * composer install
 * touch .env
 * copy over .env.example contents
 * fill in your credentials
 * leave `DB_CONNECTION=pdo_mysql` as is
-* open db.sql and copy over the db schema and import dummy data
+* import db.sql (it contains the schema and dummy data)
 * php -S localhost:8000
-* http://localhost:8000/ and try it out
-
+* open http://localhost:8000/ and try it out
 
 Added two simple tests, because they were not required but at least the testing suite is working.
-
-
 
 Demo: https://i.imgur.com/Iz6odf2.gif
--- db.sql
+++ db.sql
@@ -1,3 +1,12 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 CREATE TABLE `answer_options` (
                                   `id` int(11) NOT NULL AUTO_INCREMENT,
                                   `question_id` int(11) NOT NULL,
@@ -6,7 +15,16 @@ CREATE TABLE `answer_options` (
                                   PRIMARY KEY (`id`),
                                   KEY `question_id` (`question_id`),
                                   CONSTRAINT `answer_options_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `completes` (
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `username` varchar(255) NOT NULL,
+                             `quiz_id` int(11) NOT NULL,
+                             PRIMARY KEY (`id`),
+                             KEY `quiz_id` (`quiz_id`),
+                             CONSTRAINT `completes_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `questions` (
                              `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -15,13 +33,13 @@ CREATE TABLE `questions` (
                              PRIMARY KEY (`id`),
                              KEY `quiz_id` (`quiz_id`),
                              CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `quizzes` (
                            `id` int(11) NOT NULL AUTO_INCREMENT,
                            `name` varchar(255) NOT NULL,
                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `user_answers` (
                                 `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -36,95 +54,81 @@ CREATE TABLE `user_answers` (
                                 CONSTRAINT `user_answers_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`),
                                 CONSTRAINT `user_answers_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
                                 CONSTRAINT `user_answers_ibfk_3` FOREIGN KEY (`option_id`) REFERENCES `answer_options` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `completes` (
-                             `id` int(11) NOT NULL AUTO_INCREMENT,
-                             `username` varchar(255) NOT NULL,
-                             `quiz_id` int(11) NOT NULL,
-                             PRIMARY KEY (`id`),
-                             KEY `quiz_id` (`quiz_id`),
-                             CONSTRAINT `completes_ibfk_1` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+INSERT INTO `answer_options` (`id`, `question_id`, `option_text`, `is_correct`) VALUES
+    (1, 1, 'George Washington', 1);
+INSERT INTO `answer_options` (`id`, `question_id`, `option_text`, `is_correct`) VALUES
+    (2, 1, 'Thomas Jefferson', 0);
+INSERT INTO `answer_options` (`id`, `question_id`, `option_text`, `is_correct`) VALUES
+    (3, 1, 'Abraham Lincoln', 0);
+INSERT INTO `answer_options` (`id`, `question_id`, `option_text`, `is_correct`) VALUES
+                                                                                    (4, 2, '1945', 1),
+                                                                                    (5, 2, '1939', 0),
+                                                                                    (6, 2, '1941', 0),
+                                                                                    (7, 3, 'Paris', 1),
+                                                                                    (8, 3, 'Madrid', 0),
+                                                                                    (9, 3, 'Rome', 0),
+                                                                                    (10, 4, 'Asia', 1),
+                                                                                    (11, 4, 'Europe', 0),
+                                                                                    (12, 4, 'Africa', 0),
+                                                                                    (13, 5, 'Au', 1),
+                                                                                    (14, 5, 'Ag', 0),
+                                                                                    (15, 5, 'Cu', 0),
+                                                                                    (16, 6, 'Mars', 1),
+                                                                                    (17, 6, 'Venus', 0),
+                                                                                    (18, 6, 'Jupiter', 0),
+                                                                                    (19, 7, 'France', 1),
+                                                                                    (20, 7, 'Germany', 0),
+                                                                                    (21, 7, 'Brazil', 0),
+                                                                                    (22, 8, 'Barry Bonds', 1),
+                                                                                    (23, 8, 'Babe Ruth', 0),
+                                                                                    (24, 8, 'Hank Aaron', 0),
+                                                                                    (25, 9, 'Elon Musk', 1),
+                                                                                    (26, 9, 'Jeff Bezos', 0),
+                                                                                    (27, 9, 'Mark Zuckerberg', 0),
+                                                                                    (28, 10, 'HyperText Markup Language', 1),
+                                                                                    (29, 10, 'Home Tool for Modern Living', 0),
+                                                                                    (30, 10, 'High Tech Machine Learning', 0),
+                                                                                    (31, 11, 'Bar', 1),
+                                                                                    (32, 11, 'Boo', 0),
+                                                                                    (33, 11, 'zzz', 0);
+
+
+
+INSERT INTO `questions` (`id`, `quiz_id`, `question_text`) VALUES
+    (1, 1, 'Who was the first president of the United States?');
+INSERT INTO `questions` (`id`, `quiz_id`, `question_text`) VALUES
+    (2, 1, 'In which year did World War II end?');
+INSERT INTO `questions` (`id`, `quiz_id`, `question_text`) VALUES
+    (3, 2, 'What is the capital of France?');
+INSERT INTO `questions` (`id`, `quiz_id`, `question_text`) VALUES
+                                                               (4, 2, 'Which is the largest continent by land area?'),
+                                                               (5, 3, 'What is the chemical symbol for gold?'),
+                                                               (6, 3, 'Which planet is known as the \"Red Planet\"?'),
+                                                               (7, 4, 'Which country won the FIFA World Cup in 2018?'),
+                                                               (8, 4, 'Who holds the record for the most home runs in Major League Baseball?'),
+                                                               (9, 5, 'Who is the CEO of Tesla?'),
+                                                               (10, 5, 'What does HTML stand for?'),
+                                                               (11, 5, 'Foo');
+
+INSERT INTO `quizzes` (`id`, `name`) VALUES
+    (1, 'History Quiz');
+INSERT INTO `quizzes` (`id`, `name`) VALUES
+    (2, 'Geography Quiz');
+INSERT INTO `quizzes` (`id`, `name`) VALUES
+    (3, 'Science Quiz');
+INSERT INTO `quizzes` (`id`, `name`) VALUES
+                                         (4, 'Sports Quiz'),
+                                         (5, 'Technology Quiz');
 
 
 
 
--- Quiz 1
-INSERT INTO quizzes (name) VALUES ('History Quiz');
-
-INSERT INTO questions (quiz_id, question_text) VALUES
-                                                   (1, 'Who was the first president of the United States?'),
-                                                   (1, 'In which year did World War II end?');
-
-INSERT INTO answer_options (question_id, option_text, is_correct) VALUES
-                                                                      (1, 'George Washington', 1),
-                                                                      (1, 'Thomas Jefferson', 0),
-                                                                      (1, 'Abraham Lincoln', 0),
-                                                                      (2, '1945', 1),
-                                                                      (2, '1939', 0),
-                                                                      (2, '1941', 0);
-
--- Quiz 2
-INSERT INTO quizzes (name) VALUES ('Geography Quiz');
-
-INSERT INTO questions (quiz_id, question_text) VALUES
-                                                   (2, 'What is the capital of France?'),
-                                                   (2, 'Which is the largest continent by land area?');
-
-INSERT INTO answer_options (question_id, option_text, is_correct) VALUES
-                                                                      (3, 'Paris', 1),
-                                                                      (3, 'Madrid', 0),
-                                                                      (3, 'Rome', 0),
-                                                                      (4, 'Asia', 1),
-                                                                      (4, 'Europe', 0),
-                                                                      (4, 'Africa', 0);
-
--- Quiz 3
-INSERT INTO quizzes (name) VALUES ('Science Quiz');
-
-INSERT INTO questions (quiz_id, question_text) VALUES
-                                                   (3, 'What is the chemical symbol for gold?'),
-                                                   (3, 'Which planet is known as the "Red Planet"?');
-
-INSERT INTO answer_options (question_id, option_text, is_correct) VALUES
-                                                                      (5, 'Au', 1),
-                                                                      (5, 'Ag', 0),
-                                                                      (5, 'Cu', 0),
-                                                                      (6, 'Mars', 1),
-                                                                      (6, 'Venus', 0),
-                                                                      (6, 'Jupiter', 0);
-
--- Quiz 4
-INSERT INTO quizzes (name) VALUES ('Sports Quiz');
-
-INSERT INTO questions (quiz_id, question_text) VALUES
-                                                   (4, 'Which country won the FIFA World Cup in 2018?'),
-                                                   (4, 'Who holds the record for the most home runs in Major League Baseball?');
-
-INSERT INTO answer_options (question_id, option_text, is_correct) VALUES
-                                                                      (7, 'France', 1),
-                                                                      (7, 'Germany', 0),
-                                                                      (7, 'Brazil', 0),
-                                                                      (8, 'Barry Bonds', 1),
-                                                                      (8, 'Babe Ruth', 0),
-                                                                      (8, 'Hank Aaron', 0);
-
--- Quiz 5
-INSERT INTO quizzes (name) VALUES ('Technology Quiz');
-
-INSERT INTO questions (quiz_id, question_text) VALUES
-                                                   (5, 'Who is the CEO of Tesla?'),
-                                                   (5, 'What does HTML stand for?'),
-                                                   (5, 'Foo');
-
-INSERT INTO answer_options (question_id, option_text, is_correct) VALUES
-                                                                      (9, 'Elon Musk', 1),
-                                                                      (9, 'Jeff Bezos', 0),
-                                                                      (9, 'Mark Zuckerberg', 0),
-                                                                      (10, 'HyperText Markup Language', 1),
-                                                                      (10, 'Home Tool for Modern Living', 0),
-                                                                      (10, 'High Tech Machine Learning', 0),
-                                                                      (11, 'Bar', 1),
-                                                                      (11, 'Boo', 0),
-                                                                      (11, 'zzz', 0);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
